add0fa308b7a4ddbca694d183de6bf6cedd3946c
aaestrada <v-aaestrada@expediagroup.com>
2021-12-16 11:15:20 -0600

    Run profile test passing the pino instance at hapi-pino in the register plugins for the hapi plugin

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -5,6 +5,8 @@
  */
 
 const Catalyst = require('../..');
+const Pino = require('pino');
+const HapiPino = require('hapi-pino');
 const Path = require('path');
 const getResponse = [
   {string: 'string1', number: 1, boolean: true},
@@ -30,6 +32,56 @@ async function start (options = {}) {
       return await h.response(getResponse);
     },
   });
+
+  const transport = Pino.transport({
+    targets: [
+        {
+            level: 'info',
+            target: 'pino-pretty',
+            options: {
+                singleLine: false,
+                ignorePaths: ['/favicon.ico'],
+                logRequestComplete: false,
+                append: true,
+                mkdir: true,
+                destination: 'logs/.info.log'
+            }
+        },
+        {
+            level: 'error',
+            target: 'pino-pretty',
+            options: {
+                singleLine: false,
+                ignorePaths: ['/favicon.ico'],
+                logRequestComplete: false,
+                append: true,
+                mkdir: true,
+                destination: 'logs/.error.log'
+            }
+        },
+        {
+            level: 'warn',
+            target: 'pino-pretty',
+            options: {
+                singleLine: false,
+                ignorePaths: ['/favicon.ico'],
+                logRequestComplete: false,
+                append: true,
+                mkdir: true,
+                destination: 'logs/.warn.log'
+            }
+        }]
+});
+
+await server.register({
+    plugin: HapiPino,
+    options: {
+        instance: Pino(transport),
+        mergeHapiLogData: false,
+        ignorePaths: ['/favicon.ico'],
+        ignoredEventTags: { log: ['client'], request: '*' },
+    }
+});
   
   await server.start()
   server.log(['info'], `items endpoint : ${server.info.uri}/items`)
